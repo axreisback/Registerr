bce5315263089dd454ff5c87e858ec8731e85ecb
axreisback <86855367+axreisback@users.noreply.github.com>
2021-09-09 20:15:25 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+OwnerID: ["814610845904404511"],    
+GuildID: "",
+VoiceID: "",
+Token: "",
+Prefix: ["!", "."],
+URL: "MONGURL",
+
+Tag: "TAG",
+SecondaryTag: "•",
+
+ServerMessage: "Striga ❤️ ServerName",
+ServerCase: "dnd",
+Footer: "Striga ❤️ ServerName"
+}
